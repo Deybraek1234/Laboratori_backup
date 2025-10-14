dde2386924d43f4a2afee8dcbaedba0ee720ff79
Deybraek1234 <francigrossi05@gmail.com>
2025-10-14 23:56:03 +0200

    aggiunto macro root

---
+++ Lab 2/Root/fotodiodord.cxx
@@ -0,0 +1,26 @@
+void fotodiodord() {
+   TCanvas *c1 = new TCanvas("c1","Bruciamo le lampadine :) (graph 2)",1600,1000);
+   c1->SetGrid();
+   c1->GetFrame()->SetBorderSize(12);
+  
+   TGraphErrors *gr = new TGraphErrors("fotodiodord.txt","%lg %lg");
+   gr->SetTitle("Voltagio vs Rd");
+   gr->SetMarkerColor(2);
+   gr->SetMarkerStyle(5);
+   gr->SetLineColor(9);
+   gr->SetMarkerSize(1.5);
+
+   gr->GetXaxis()->SetTitle("Potenziale (V)");
+   gr->GetXaxis()->CenterTitle(true);
+   gr->GetXaxis()->SetTitleSize(0.045);
+
+   gr->GetYaxis()->SetTitle("Resistenza Dinamica (R)");
+   gr->GetYaxis()->CenterTitle(true);
+   gr->GetYaxis()->SetTitleSize(0.045);
+
+   gr->Draw("AP");
+
+   gr->Draw("AP");
+   c1->Update();
+   c1->SaveAs("fotodiodord.jpg");
+}
--- Lab 2/Root/grafico2.cxx
+++ Lab 2/Root/grafico2.cxx
@@ -9,28 +9,55 @@ void grafico2() {
    gr->SetMarkerStyle(5);
    gr->SetLineColor(9);
    gr->SetMarkerSize(1.5);
+
    gr->GetXaxis()->SetTitle("Potenziale (V)");
    gr->GetXaxis()->CenterTitle(true);
+   gr->GetXaxis()->SetTitleSize(0.045);
+
    gr->GetYaxis()->SetTitle("Corrente (A)");
    gr->GetYaxis()->CenterTitle(true);
+   gr->GetYaxis()->SetTitleSize(0.045);
+
    gr->Draw("AP");
 
-   //const int N_Points = 14;
+   //create fake graph to do unweighted plot 
+   TGraph *g1 = new TGraph("grafico2.txt","%lg %lg");
+   //numero di punti
+   const int N_Points = 14;
    //Fit del grafico
-   //Double_t x_fit_start = gr->GetX()[0];
-   //Double_t x_fit_end = gr->GetX()[N_Points-1];
-   //Double_t x_plot_start = gr->GetXaxis()->GetXmin();
-   //Double_t x_plot_end = gr->GetXaxis()->GetXmax();
+   double x_fit_start = g1->GetX()[0];
+   double x_fit_end = g1->GetX()[N_Points-1];
+   double x_plot_start = g1->GetXaxis()->GetXmin();
+   double x_plot_end = g1->GetXaxis()->GetXmax();
+   TF1 *fit_func = new TF1("fit_func", "pol1", x_plot_start, x_plot_end);   
+   g1->Fit(fit_func, "R", "", x_fit_start, x_fit_end);
+   //per parametri fit
+   double intercept = fit_func->GetParameter(0);
+   double slope = fit_func->GetParameter(1);
+   TString fit_label = TString::Format(
+    "#splitline{Fit Lineare:}{y = (%.3e) + (%.3e)x}",
+    intercept, slope
+);
 
-   //TF1 *fit_func = new TF1("fit_func", "pol1", x_plot_start, x_plot_end);   
-   //gr->Fit(fit_func, "R", "", x_fit_start, x_fit_end);
-   
-   TF1 *line = new TF1("line_func", "[0]+[1]*x", -0.099998, 1.5);
-   line->SetParameter(0,0.001069);
-   line->SetParameter(1,0.4708);
-   line->SetLineColor(49);
-   gr->Draw("AP");
-   line->Draw("SAME");
+
+   //linea accensione lampadina
+   double y_min = gr->GetYaxis()->GetXmin();
+   double y_max = gr->GetYaxis()->GetXmax();
+   double x_point = 0.331;
+   TLine* line = new TLine(x_point, y_min, x_point, y_max);
+   line->SetLineColor(34);
+   line->SetLineWidth(2);
+   line->Draw();
+
+   //legenda
+   TLegend *Tl = new TLegend(0.1, 0.9, 0.35, 0.75);
+   Tl->AddEntry(gr, "Dati Misurati", "ep");
+   Tl->AddEntry(fit_func, fit_label, "l");
+   Tl->AddEntry(line, "Accensione Lampadina", "l");
+   Tl->Draw();
+
+   //draw everything
+   fit_func->Draw("SAME");
    c1->Update();
-   c1->SaveAs("graficozoomato.jpg");
+   c1->SaveAs("graficozoomato1.jpg");
 }
