5fd55246ce5661adebea9a7c059520013ac1e915
Deybraek1234 <francigrossi05@gmail.com>
2025-10-18 17:41:13 +0200

    Automated backup: 2025-10-18 17:41:06

---
+++ Lab 2/Root/fit_grafico_cxx.d
@@ -0,0 +1,4 @@
+
+# DO NOT DELETE
+./fit_grafico_cxx.so: /usr/include/RVersion.h /usr/include/ROOT/RConfig.hxx /usr/include/TClass.h /usr/include/TDictAttributeMap.h /usr/include/TInterpreter.h /usr/include/TROOT.h /usr/include/TBuffer.h /usr/include/TMemberInspector.h /usr/include/TError.h /usr/include/RtypesImp.h /usr/include/TIsAProxy.h /usr/include/TFileMergeInfo.h /usr/include/TCollectionProxyInfo.h /usr/bin/rootcling
+fit_grafico_cxx__ROOTBUILDVERSION= 6.36.04
--- Lab 2/Root/grafico_no_fit_partial.cxx
+++ Lab 2/Root/grafico_no_fit_partial.cxx
@@ -1,6 +1,6 @@
 #include<string>
 
-void fit_grafico() {
+void grafico_no_fit() {
 
    //nome file grafico
    //const TString nome_grafico = "grafico";
@@ -16,6 +16,10 @@ void fit_grafico() {
 
    //number of points
    const int number_point = 99999;
+
+   int number_of_points;
+   std::cout <<"Quanti Punti Graficare"?;
+   std::cin  >> number_of_points;
 
    //initialize array and counter for loading array
    double x[number_point];
@@ -36,7 +40,7 @@ void fit_grafico() {
    cv->SetGrid();
    cv->GetFrame()->SetBorderSize(12);
 
-   TGraphErrors *g1 = new TGraphErrors(n, x, y, errx, erry);
+   TGraphErrors *g1 = new TGraphErrors(number_of_points, x, y, errx, erry);
    g1->SetTitle(nome_grafico);
    //punti
    g1->SetMarkerColor(2);  //red
@@ -46,58 +50,19 @@ void fit_grafico() {
    g1->GetXaxis()->SetTitle("Placeholder");
    g1->GetXaxis()->CenterTitle(true);
    //g1->GetXaxis()->SetTitleSize();
+
    //yaxis
    g1->GetYaxis()->SetTitle("Placeholder");
    g1->GetYaxis()->CenterTitle(true);
    //g1->GetYaxis()->SetTitleSize();
 
-   //per fare il fit (unweighted)
-   int fit_tutti_punti;
-   std::cout <<"Fare fit tutti punti?[1-si/0-custom range]";
-   std::cin >> fit_tutti_punti;
-   TGraph *gfake = new TGraph(n, x, y);
-
-   int i;
-   int j;
-   double x_start;
-   double x_end;
-
-//if statement for checking if fit all data
-   if(fit_tutti_punti == 1){
-   x_start = x[0];
-   x_end = x[number_point-1];
-} else if (fit_tutti_punti == 0){
-   std::cout <<"Numero di punto iniziale:";
-   std::cin >> i;
-   x_start = x[i-1];
-   std::cout <<"Numero di punto Finale:";
-   std::cin >> j;
-   x_end = x[j-1];
-} else {
-   exit(1);
-}
-
-   TF1 *fit_func = new TF1("fit_func", "pol1", g1->GetXaxis()->GetXmin(), g1->GetXaxis()->GetXmax());
-   gfake->Fit(fit_func, "R", "", x_start, x_end);
-   double slope = fit_func->GetParameter(1);
-   double intercept = fit_func->GetParameter(0);
-   fit_func->SetLineColor(48);
-
-   //pendenza e intercetta per legenda
-   TString fit_label = TString::Format(
-    "#splitline{Fit Lineare:}{y = (%.3e)x + (%.3e)}",
-    slope, intercept
-   );
-
    //legenda
-   TLegend *Tl = new TLegend(0.1, 0.9, 0.35, 0.75);
+   TLegend *Tl = new TLegend(0.1,0.9,0.35, 0.75)
    Tl->AddEntry(g1, "Dati Misurati", "ep");
-   Tl->AddEntry(fit_func, fit_label, "ep");
+   
 
    //draw everything
    g1->Draw("SAME AP");
-   fit_func->Draw("SAME");
-   Tl->Draw("SAME");
    cv->Update();
    cv->SaveAs((nome_grafico + ".eps").Data());
 }
